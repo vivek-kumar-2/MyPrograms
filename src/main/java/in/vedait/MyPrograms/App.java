7a2e7116285b814b6bf965e369df4397317c9f65
Vivek Kumar <vivekkumarvk0037@gmail.com>
2023-10-19 12:27:59 +0530

    Add File

---
+++ src/main/java/in/vedait/MyPrograms/App.java
@@ -0,0 +1,13 @@
+package in.vedait.MyPrograms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
